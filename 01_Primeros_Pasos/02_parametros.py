e5be7d7d3e5dcd2867cb8820c3a694bdef315dea
skril349 <tvives.cabaleiro@gmail.com>
2025-11-05 16:30:17 +0100

    adding fixed route before dinamic route

--- 01_Primeros_Pasos/02_parametros.py
+++ 01_Primeros_Pasos/02_parametros.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/books/favourite")
+async def get_favourite():
+    return{"favourite book": "game of thrones"}
+
 @app.get("/books/{book_id}")
-async def get_book(book_id: int):
+async def get_book(book_id: str):
     return {"book_id":book_id}
