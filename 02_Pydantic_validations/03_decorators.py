1afa369bd0d318d8d55fe1aa1e319be276e6a09c
skril349 <tvives.cabaleiro@gmail.com>
2025-11-06 15:52:15 +0100

    validations with field_validator

---
+++ 02_Pydantic_validations/03_decorators.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel, AfterValidator, field_validator
+from typing import Annotated
+from fastapi import FastAPI
+
+class Item(BaseModel):
+    item_id: int
+    price: float
+
+    @field_validator("item_id")
+    def check_positive(cls,value:int):
+        if value < 0:
+            raise ValueError("Item ID debe ser positivo")
+        return value
+
+banana: Item = Item(item_id = 2, price = 2.6)
+
