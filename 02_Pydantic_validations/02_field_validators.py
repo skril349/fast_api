019b86296ddd907b7cfa326be0fa2b6d3960ce6c
skril349 <tvives.cabaleiro@gmail.com>
2025-11-06 15:45:30 +0100

    afterValidators

--- 02_Pydantic_validations/02_field_validators.py
+++ 02_Pydantic_validations/02_field_validators.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import AfterValidator, BaseModel
 from typing import Annotated
 
-
+# ================== ANNOTATED ========================
 def es_par(value:int) -> int:
     if value % 2 ==1:
         raise ValueError(f"{value} no es numero par")
@@ -19,11 +19,19 @@ class Model1(BaseModel):
 
 ejemplo:Model1 = Model1(my_number = 2)
 
-# class Model2(BaseModel):
-#     my_number:NumeroPar
+class Model2(BaseModel):
+    other_number :Annotated[NumeroPar,AfterValidator(lambda v: v + 2)]
 
-# class Model3(BaseModel):
-#     my_number:NumeroPar
+
+ejemplo2:Model2 = Model2(other_number = 4)
+print(ejemplo2)
+
+
+class Model3(BaseModel):
+    lista_pares:list[NumeroPar]
+
+ejemplo3:Model3 = Model3(lista_pares=[2,6,10])
+print(ejemplo3)
 
 # Before: corre antes de validaciones/transformaciones de pydantic
 # Plain: Similar a before pero termina la validacion al retur del valor
