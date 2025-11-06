1e2714cb5c3e384d1f3613248f21897bafeb2541
skril349 <tvives.cabaleiro@gmail.com>
2025-11-06 17:14:57 +0100

    query strings validations

--- 03_Fastapi_Validaciones/01_query_validations.py
+++ 03_Fastapi_Validaciones/01_query_validations.py
@@ -3,9 +3,16 @@ from typing import Annotated
 
 app = FastAPI()
 
+# STRINGS VALIDATIONS
+# max_length
+# min_length
+# pattern
+
 @app.get("/items/")
-async def read_items(q: Annotated[list[str] | None,Query()] = None):
+async def read_items(q: Annotated[str | None,Query(min_length=3, max_length=20)] = None):
     results: dict = {"mensaje": "Acceso a get(read_items)"}
     if q:
         results.update({"q":q})
     return results
+
+
