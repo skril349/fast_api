81e60eee97e169c24500023d40faceafd75baef5
skril349 <tvives.cabaleiro@gmail.com>
2025-11-10 08:57:51 +0100

    adding prefix, tags and responses

--- 10_Routers/app/routers/user.py
+++ 10_Routers/app/routers/user.py
@@ -1,12 +1,21 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/users",
+    tags=["USERS"],
+    responses={404: {"Description":"Not found"}}
+)
 
 users_list = [
     {"id":1, "name":"Toni", "lastname":"Vives"},
     {"id":2, "name":"Marc", "lastname":"garcia"}
 ]
 
-@router.get("/users/")
+@router.get("/", tags=["Get all"])
 async def get_users():
     return users_list
+
+
+@router.get("/{user_id}", tags=["Get user by id"])
+async def get_user_by_id(user_id:int):
+    return next((user for user in users_list if user["id"] == user_id), {"error":"user not found"})
