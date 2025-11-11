0c2217381ddf389a2b240ff945b770cea5e7660b
skril349 <tvives.cabaleiro@gmail.com>
2025-11-11 09:09:47 +0100

    adding dependencies

--- 05_Seguridad/01_Depends.py
+++ 05_Seguridad/01_Depends.py
@@ -3,6 +3,24 @@ from typing import Annotated
 
 app = FastAPI()
 
+class AuthService:
+    def authenticate(self, token:str):
+        if token == "Valid-token":
+            return True
+        else:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+def get_auth_service():
+    return AuthService()
+
+class EmailService:
+    def send_email(self, recipient:str, message:str):
+        print(f"Sending email to {recipient}: {message}")
+
+def get_email_service():
+    return EmailService()
+
+
 class Logger:
     def log(self,message:str)-> None:
         print(f"logging message:{message}")
@@ -10,9 +28,10 @@ class Logger:
 def get_logger():
     return Logger()
 
+
 logger_dependency = Annotated[Logger,Depends(get_logger)]
-
-
+email_service_dependency = Annotated[EmailService, Depends(get_email_service)]
+auth_service_dependency = Annotated[AuthService, Depends(get_auth_service)]
 
 @app.get("/items/{message}")
 async def get_items(message:str, logger:logger_dependency):
@@ -23,3 +42,13 @@ async def get_items(message:str, logger:logger_dependency):
 async def get_products(message:str, logger:logger_dependency):
     logger.log(message)
     return message
+
+def send_email(recipient:str, message:str, email_service: email_service_dependency):
+    email_service.send_email(recipient, message)
+
+@app.get("/secure-data/")
+def get_secure_data(token:str, auth_service: auth_service_dependency):
+    if auth_service.authenticate(token):
+        return "authenticated"
+    else:
+        return "unauthenticated"
